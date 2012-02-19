0b3dc771b841432adc8a7910f68e28ab14854e3c
Matthias Kretschmann <desk@kremalicious.com>
2012-02-19 07:06:57 +0100

    Renaming docs->index, index->template; Docs: - started Quick Start block - added jquery & script files - added Responsive & Touch Enabled Image Slider - banner fixes

---
+++ html/assets/js/script.js
@@ -0,0 +1,12 @@
+
+$(window).load(function() { // after the DOM has loaded
+	
+	// Initiate FlexSlider
+	// http://flex.madebymufffin.com/
+	////////////////////////////////////////////////
+ 	$('.flexslider').flexslider({
+ 		animation: "slide", 
+ 		slideshow: false
+ 	});
+ 	
+});
